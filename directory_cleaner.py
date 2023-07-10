d4956cbf5b510451ed658f01d1f31e1e5ba38b0a
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-10 18:04:33 -0400

    Shortened code significantly for fun

--- directory_cleaner.py
+++ directory_cleaner.py
@@ -4,33 +4,25 @@ import shutil
 from datetime import datetime as dt
 from pathlib import Path
 
-
 class DirectoryCleaner:
-    get_time = lambda _: dt.now().strftime('%I-%M-%S%p %m:%d:%Y')
-    time = get_time(1)
     def __init__(self, src_path, dest_path, move_date):
         self.src_path = Path(src_path)
         self.dest_path = Path(dest_path)
         self.move_date = dt.strptime(move_date, '%Y-%m-%d')
         self.make_dir()
-    
-    def make_dir(self):
-        os.makedirs(Path(self.dest_path) / self.time, exist_ok=True)
-    
-    def move_files(self, file):
-        shutil.move(self.src_path / file, self.dest_path / self.time / file)
-    
-    def clean(self):
-        files = list(filter(lambda i: self.move_files(i) if dt.fromtimestamp(os.path.getmtime(self.src_path / i))<=self.move_date else False,os.listdir(self.src_path)))
 
+    make_dir = lambda self: os.makedirs(self.dest_path / self.time, exist_ok=True)
+    move_files = lambda self, file: shutil.move(self.src_path / file, self.dest_path / self.time / file)
+    clean = lambda self: [self.move_files(file) for file in os.listdir(self.src_path) if dt.fromtimestamp(os.path.getmtime(self.src_path / file)) <= self.move_date]
 
+    time = dt.now().strftime('%I-%M-%S%p %m:%d:%Y')
 
 def main():
     conf = json.load(open(Path(__file__).parent.absolute() / 'config.json'))
-    src_path = conf['src_dir']
-    dest_path = conf['dest_dir']
-    move_date = conf['move_before_date']
-    cleaner = DirectoryCleaner(src_path, dest_path, move_date).clean()
+    DirectoryCleaner(conf['src_dir'], conf['dest_dir'], conf['move_before_date']).clean()
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except FileNotFoundError as e:
+        print(f'FileNotFoundError: Check the config.json file {str(e)}')
