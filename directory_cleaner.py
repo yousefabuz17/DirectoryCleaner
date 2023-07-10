6d4c93347dd146b1a4509349b6671b572e44842e
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-10 18:44:34 -0400

    Runs on the first day of every month

--- directory_cleaner.py
+++ directory_cleaner.py
@@ -14,23 +14,24 @@ class DirectoryCleaner:
 
     make_dir = lambda self: os.makedirs(self.dest_path / self.time, exist_ok=True)
     move_files = lambda self, file: shutil.move(self.src_path / file, self.dest_path / self.time / file)
-    is_ignored = lambda self, file: any(entry.lower() in file.lower() for entry in self.ignored_files)
+    is_ignored = lambda self, file: any(entry in file for entry in self.ignored_files)
     clean = lambda self: [self.move_files(file) for file in os.listdir(self.src_path) if dt.fromtimestamp(os.path.getmtime(self.src_path / file)) <= self.move_date and not self.is_ignored(file) and not os.path.splitext(file)[1]=='']
 
     time = dt.now().strftime('%I-%M-%S%p %m:%d:%Y')
 
 def main():
     conf = json.load(open(Path(__file__).parent.absolute() / 'config.json'))
-    directories = conf['directories']
+    directories = conf.get('directories', [])
     ignored_files = [entry.lower() for entry in conf.get('.ignore', [])]
     for directory in directories:
         DirectoryCleaner(directory['src_dir'], directory['dest_dir'], directory['move_before_date'], ignored_files).clean()
 
 if __name__ == '__main__':
-    # TODO: Only run on the first day of the month
-    try:
-        main()
-    except FileNotFoundError as e:
-        print(f'FileNotFoundError: Change move_date on config.json to a date before {dt.fromtimestamp(os.path.getmtime(Path(__file__).parent.absolute() / "config.json"))}')
-    except ValueError as e:
-        print(f'ValueError: {str(e)}')
+    if dt.now().day == 1:
+        try:
+            main()
+        except FileNotFoundError as e:
+            print(f'FileNotFoundError: Change move_date on config.json to a date before {dt.fromtimestamp(os.path.getmtime(Path(__file__).parent.absolute() / "config.json"))}')
+        except ValueError as e:
+            print(f'ValueError: {str(e)}')
+    else:exit()
